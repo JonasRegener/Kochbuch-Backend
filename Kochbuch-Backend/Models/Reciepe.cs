5b185c6dcaecc97dafa44e3bb91a61245b6188f8
JonasRegener <regener.jonas@gmail.com>
2024-02-27 19:37:27 +0100

    Projektdateien hinzufügen.

---
+++ Kochbuch-Backend/Models/Reciepe.cs
@@ -0,0 +1,11 @@
+﻿namespace Kochbuch_Backend.Models
+{
+    public class Reciepe
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }    
+        public string? Description { get; set; }
+        public int DurationMinutes { get; set; } 
+         
+    }
+}
